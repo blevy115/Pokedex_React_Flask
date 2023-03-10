8995491e365534dab1574f638a0fa19d83c00583
Brandon Levy <brandon.levy@mail.mcgill.ca>
2023-03-10 16:06:25 -0500

    Set up Apollo client url and first query to grab Pokemon list

--- pokedex-react-flask-app/src/App.js
+++ pokedex-react-flask-app/src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import { useQuery, gql } from "@apollo/client"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const GEN_1_QUERY = gql`
+query { gen_1: pokemon_v2_pokemonspecies(where: {generation_id: {_eq: 1}}) {
+    id,
+    name
+  }
+}`
+
+export default function App() {
+  const {data, loading, error} = useQuery(GEN_1_QUERY)
+  if (loading) return "Loading..."
+  if(error) return <pre>{error.message}</pre>
+
+  const { gen_1 } = data
+  console.log(gen_1)
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +36,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- pokedex-react-flask-app/src/index.js
+++ pokedex-react-flask-app/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: "https://beta.pokeapi.co/graphql/v1beta/",
+  cache: new InMemoryCache()
+
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
