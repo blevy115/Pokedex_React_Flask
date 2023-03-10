9d7c5999c91c7045b83286090f51944376dcb9ff
Brandon Levy <brandon.levy@mail.mcgill.ca>
2023-03-10 18:19:26 -0500

    Added Pokemon Generation Changer and made it changing it update the list

--- pokedex-react-flask-app/src/App.js
+++ pokedex-react-flask-app/src/App.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useQuery, gql } from "@apollo/client"
 import './App.css';
 
-const GEN_1_QUERY = gql`
-query { gen_1: pokemon_v2_pokemonspecies(
-    where: {generation_id: {_eq: 1}},
-    order_by: {id: asc}
-  ) {
-    id,
-    name
+
+function getPokemonGeneration(id) {
+  return gql`
+  query { pokemon_list: pokemon_v2_pokemonspecies(
+      where: {generation_id: {_eq: ${id || 1}}},
+      order_by: {id: asc}
+    ) {
+      id,
+      name
+    }
+  }`
+}
+
+function getGenerationList() {
+  return gql`
+  query { generations: pokemon_v2_generation(distinct_on: id) {
+    id
   }
-}`
+  }`
+}
 
 export default function App() {
-  const {data, loading, error} = useQuery(GEN_1_QUERY)
+  const [generation, setGeneration] = useState()
+  const {data: generations} = useQuery(getGenerationList())
+  useEffect(() => {
+    if (generations?.generations)  {
+      setGeneration(generations[0])
+    }
+  }, [])
+
+  const {data, loading, error} = useQuery(getPokemonGeneration(generation))
   if (loading) return "Loading..."
   if(error) return <pre>{error.message}</pre>
 
-  const { gen_1 } = data
+  const { pokemon_list } = data
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Gen 1 Pokemon</h2>
-        { gen_1 ? (
+        <h2>Gen {generation} Pokemon</h2>
+        <select id="generation"
+        onChange={(e) => setGeneration(e.target.value)}
+          >
+          Generation
+        {generations?.generations.map(({id}) => (<option key={id} value={id}>{id}</option>))}
+        </select>
+        { pokemon_list ? (
           <ul>
-            {gen_1.map(({name, id}) => (<li key={id}>{name}</li>))}
+            {pokemon_list.map(({name, id}) => (<li key={id}>{name}</li>))}
           </ul>
         ) : undefined }
       </header>
