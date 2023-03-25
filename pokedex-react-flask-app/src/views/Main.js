310fded3d2b921b9380c0a8c4e872ca270e45778
Brandon Levy <brandon.levy@mail.mcgill.ca>
2023-03-25 13:50:30 -0400

    Fixed Type Chart and made input field focus on load

--- pokedex-react-flask-app/src/views/Main.js
+++ pokedex-react-flask-app/src/views/Main.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_POKEMON_LIST } from "../api/pokeapi";
 import PokemonList from "../components/PokemonList";
+
 
 export default function Main() {
   const [name, setName] = useState("");
@@ -9,11 +10,19 @@ export default function Main() {
     variables: { name: `%${name}%` },
     skip: !name,
   });
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Search For Pokemon</p>
         <input
+          ref={inputRef}
           value={name}
           onChange={(val) => setName(val.target.value)}
         ></input>
