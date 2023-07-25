bd8cdbeac36ed5c7a6d0e6336376bc8dc2242e3e
blevy115 <32915571+blevy115@users.noreply.github.com>
2023-07-25 15:59:09 -0400

    Added Missingno image for when sprite src path does not exist

--- pokedex-react-flask-app/src/helpers/error.js
+++ pokedex-react-flask-app/src/helpers/error.js
@@ -1,3 +1,5 @@
 export function handleImageError(ele) {
-  ele.target.style.display = "none";
+  ele.target.src = "/icons/error/missingno.png";
+  ele.target.height = 96;
+  ele.target.width = 96;
 }
