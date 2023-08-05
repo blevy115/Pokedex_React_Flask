f6bca5a62947a4449e5d4d76fe851da138fa0304
blevy115 <32915571+blevy115@users.noreply.github.com>
2023-08-05 18:59:30 -0400

    Added json form gmax and max moves

---
+++ pokedex-react-flask-app/src/helpers/getMaxMoves.js
@@ -0,0 +1,20 @@
+import gmax_moves from "../data/gmax_moves.json";
+// import max_moves from "../data/max_moves.json";
+
+export function isGmaxMove(id) {
+    return gmax_moves[id]
+}
+
+export function getGmaxMove(pokemonId) {
+  for (const moveId in gmax_moves) {
+    const pokemonList = gmax_moves[moveId].pokemon;
+    if (pokemonList?.some((pokemon) => pokemon.id === pokemonId)) {
+      const move = gmax_moves[moveId];
+      return {
+        ...move,
+        id: moveId,
+      };
+    }
+  }
+  return null;
+}
